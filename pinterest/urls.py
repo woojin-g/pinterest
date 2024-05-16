3aa98e0181097055e28ffb004830e421f9d6533a
woojin-g <blueofficial0119@gmail.com>
2024-05-17 00:26:24 +0900

    프로젝트 경로 수정

--- pinterest/urls.py
+++ pinterest/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for playground_django project.
+URL configuration for pinterest project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
